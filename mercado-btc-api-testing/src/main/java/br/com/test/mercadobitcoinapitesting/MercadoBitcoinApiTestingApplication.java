9bd719f17f40ea2b4783e8133fa07f38fb526657
matheus gomes <matheus.gomes@southsystem.com.br>
2019-11-09 20:42:19 -0300

    subindo arquivos do projeto

---
+++ mercado-btc-api-testing/src/main/java/br/com/test/mercadobitcoinapitesting/MercadoBitcoinApiTestingApplication.java
@@ -0,0 +1,15 @@
+package br.com.test.mercadobitcoinapitesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class MercadoBitcoinApiTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoBitcoinApiTestingApplication.class, args);
+	}
+
+}
---
+++ mercado-btc-api-testing/src/test/java/br/com/test/mercadobitcoinapitesting/MercadoBiticoinApiTesting.java
@@ -0,0 +1,136 @@
+package br.com.test.mercadobitcoinapitesting;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.test.mercadobitcoinapitesting.domain.TestReport;
+import br.com.test.mercadobitcoinapitesting.repository.TestReportRepository;
+import io.restassured.http.ContentType;
+/**
+ * Classe para testes de enpoints da API publica
+ * do Mercado Bitcoin.
+ * 
+ * A documentação referência utilizada para construção destes testes
+ * está disponível em:  https://www.mercadobitcoin.com.br/api-doc/
+ * @author Matheus Adejair
+ *
+ */
+@SpringBootTest
+public class MercadoBiticoinApiTesting {
+	
+	@Autowired
+	private TestReportRepository repository;
+
+	
+	private final String BASE_URL = "https://www.mercadobitcoin.net/api/";
+	
+	/**
+	 * Teste que verifica se o método Get do endpoint /BTC/ticker retorna HttpStatus 200 OK
+	 * e ResponseBody válido.
+	 */
+	@Test
+	public void whenGetInBtcTickerThenExpect200OkAndValidResponseBody() {
+		TestReport report  = new TestReport();
+		report.setTestName("whenGetInBtcTickerThenExpect200OkAndValidResponseBody");
+
+		report.setStartedAt(LocalDateTime.now());
+		String btcPath = "BTC/ticker/";
+		given()
+			.relaxedHTTPSValidation()
+			.when()
+			.get(BASE_URL + btcPath)
+			.then()
+			.statusCode(is(200))
+			.contentType(ContentType.JSON)
+			.body("ticker.high", is(anything()))
+			.body("ticker.low", is(anything()))
+			.body("ticker.vol", is(anything()))
+			.body("ticker.last", is(anything()))
+			.body("ticker.buy", is(anything()))
+			.body("ticker.sell", is(anything()))
+			.body("ticker.date", is(anything()));
+		report.setStatus("OK");
+		report.setFinishedAt(LocalDateTime.now());
+		this.repository.save(report);
+	}
+	
+	/**
+	 * Teste que verifica se o método Get do endpoint /BTC/ticker retorna HttpStatus 
+	 * 405Method Not Allowed
+	 */
+	@Test
+	void whenPostInBtcTickerThenExpect405MethodNotAllowed() {
+		TestReport report  = new TestReport();
+		report.setTestName("whenPostInBtcTickerThenExpect405MethodNotAllowed");
+
+		report.setStartedAt(LocalDateTime.now());
+		String btcPath = "BTC/ticker/";
+		given()
+			.relaxedHTTPSValidation()
+			.when()
+			.post(BASE_URL + btcPath)
+			.then()
+			.statusCode(is(405));
+		report.setStatus("OK");
+		report.setFinishedAt(LocalDateTime.now());
+		this.repository.save(report);
+	}
+	
+	/**
+	 * Teste que verifica o método GET do endpoint do livro de ofertas de Bitcoin (/BTC/orderbook/)
+	 * espera-se que HttpStatus 200 Ok  
+	 */
+
+	@Test
+	void whenGetBtcOrderBookThenExpect200OkAndValidResponseBody() {
+		TestReport report  = new TestReport();
+		report.setTestName("whenGetBtcOrderBookThenExpect200OkAndValidResponseBody");
+
+		report.setStartedAt(LocalDateTime.now());
+		String btcPath = "BTC/orderbook/";
+		given()
+			.relaxedHTTPSValidation()
+			.when()
+			.get(BASE_URL + btcPath)
+			.then()
+			.statusCode(is(200))
+			.contentType(ContentType.JSON)
+			.body("asks", is(anything()));
+		
+		report.setStatus("OK");
+		report.setFinishedAt(LocalDateTime.now());
+		this.repository.save(report);
+	}
+	
+	/**
+	 * Teste que verifica o método POST do endpoint do livro de ofertas de Bitcoin (/BTC/orderbook/)
+	 * espera-se que HttpStatus 405 Method Not Allowed  
+	 */
+	@Test
+	void whenPostInBtcOrderBookThenExpect405MethodNotAllowed() {
+		
+		
+		TestReport report  = new TestReport();
+		report.setTestName("whenPostInBtcOrderBookThenExpect405MethodNotAllowed");
+
+		report.setStartedAt(LocalDateTime.now());
+		String btcPath = "BTC/orderbook/";
+		given()
+			.relaxedHTTPSValidation()
+			.when()
+			.post(BASE_URL + btcPath)
+			.then()
+			.statusCode(is(405));
+		
+		report.setStatus("OK");
+		report.setFinishedAt(LocalDateTime.now());
+		this.repository.save(report);
+	}
+}
